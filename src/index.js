ad9d3387a9e56e753c17575f99dd1dfacdfa07c9
Rob Eisenberg <rob@bluespire.com>
2024-08-07 12:30:24 -0400

    chore: configure handlebars as view engine for express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import * as path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
+import { engine } from "express-handlebars";
 
-const app = express();
 const port = 3000;
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("home");
 });
 
 app.listen(port, () =>  console.log(`App listening on port ${port}`));
