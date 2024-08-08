2cdf2530a8df01c431ffa25fc4b674b04fc4aadb
Rob Eisenberg <rob@bluespire.com>
2024-08-08 12:45:05 -0400

    feat: basic incorporation of data

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,14 @@
+import filmSummaries from "./data/film-summaries.js";
+
 const routes = {
   "/": (req, res) => {
-    res.render("home");
-  }
+    res.render("home", { filmSummaries });
+  },
+
+  "/film/:id": (req, res) => {
+    const film = filmSummaries.find(x => x.id == req.params.id);
+    res.render("film", { film });
+  },
 };
 
 export function configureRoutes(app) {
